675d627ef1f3e91b772317c739d0c72e40d02f6d
Tom Palmer <remlapmot@hotmail.com>
2025-10-18 06:42:28 -0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 uv venv --python 3.14
 uv init
 source .venv/bin/activate
-uv add jupyterlab nbdev importlib_resources numpy
+uv add jupyterlab nbdev numpy
 nbdev_install_hooks
 uv pip install -e .
 jupyter notebook nbs/09_magics.ipynb
