f14583a9569b41a1b6f5ce8293a9c8258a72594b
Tom Palmer <remlapmot@hotmail.com>
2025-10-20 06:29:31 -0300

    Fix import

--- nbstata/magics.py
+++ nbstata/magics.py
@@ -14,7 +14,7 @@ import nbstata.browse as browse
 from fastcore.basics import patch_to
 import re
 import urllib
-from nbstata._resources import resource_path
+from ._resources import resource_path
 from bs4 import BeautifulSoup as bs
 import configparser
 
